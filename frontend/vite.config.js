60663010a69da630d2c22480d617e74ab41e7a68
AsmaAbbasi789 <109727279+AsmaAbbasi789@users.noreply.github.com>
2024-10-18 23:25:05 +0500

    Update vite.config.js 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api" : {
-        target : "http://23.23.138.156:5000",
+        target : "http://34.239.182.9:5000",
       }
     }
   }
